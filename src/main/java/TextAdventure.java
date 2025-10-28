f717cd4c40b1ee61e44b5d4cc2a3e2a7bd37c4d2
CooperStannard <03cooper.stannard@kamsc.org>
2025-10-28 14:00:22 -0400

    Update TextAdventure.java

--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 
-public class TextAdventure {
-    public static void main(String[] args) {
-System.out.println(
-        "    _    ____   ____ ___ ___      _         _        _             _     _           \n" +
-        "   / \\  / ___| / ___|_ _|_ _|    / \\   _ __| |_     / \\   _ __ ___| |__ (_)_   _____ \n" +
-        "  / _ \\ \\___ \\| |    | | | |    / _ \\ | '__| __|   / _ \\ | '__/ __| '_ \\| \\ \\ / / _ \\\n" +
-        " / ___ \\ ___) | |___ | | | |   / ___ \\| |  | |_   / ___ \\| | | (__| | | | |\\ V /  __/\n" +
-        "/_/   \\_\\____/ \\____|___|___| /_/   \\_\\_|   \\__| /_/   \\_\\_|  \\___|_| |_|_| \\_/ \\___|");
+public class TextAdventure
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // need to put title here ASCII of name
+        System.out.println();
+
+        // scene 1 getting out of the forest
+        System.out.println("\n\nYour wake up in the middle of a forest with no memory of who you are or where you are.");
+        System.out.println("You decide to travel in a random direction untile you reach something or you parish.");
+        System.out.println("Your travel for a while until you find a path. the path branches multiple tiles so chose which direction to travel. \n(left-L right-R straight-S): ");
+        for (int i=3; i>=0; i--)
+        {
+            String S = 
+        }
+
+
+
+
+
+
     }
 }
