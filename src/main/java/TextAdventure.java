651cd3bef2a8c947e52d558bb0b56f07aab4b3d8
CooperStannard <03cooper.stannard@kamsc.org>
2025-10-30 09:36:43 -0400

    Update TextAdventure.java

--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -13,11 +13,19 @@ public class TextAdventure
         System.out.println("\n\nYour wake up in the middle of a forest with no memory of who you are or where you are.");
         System.out.println("You decide to travel in a random direction untile you reach something or you parish.");
         System.out.println("Your travel for a while until you find a path. the path branches multiple tiles so chose which direction to travel. \n(left-L right-R straight-S): ");
+
+        // 
         for (int i=3; i>=0; i--)
         {
-            String S = 
+        String s = sc.nextLine();
+        char secret_Exit_ending =  s.charAt(0);
         }
+        System.out.println(secret_Exit_ending);
+        if ()
 
+        System.out.println("You travel through the woods untile you find a door in a cliff");
+        System.out.println("The door is locked when you try to open it you see a key hole");
+        System.out.println("you wonder what treasures you could be inside of the door");
 
 
 
